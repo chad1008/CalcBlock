cbaa22b577a82090ce90870e15db052d16420177
Chad Chadbourne <13856531+chad1008@users.noreply.github.com>
2021-09-08 09:40:12 -0400

    remove build folder tracking

--- blocks/calcblock/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '113c7e01f7a936874939238dd8271f55');
