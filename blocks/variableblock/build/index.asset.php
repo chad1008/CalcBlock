1ce899f0e69822a98520898b2a6dae70c9a8dacd
Chad Chadbourne <13856531+chad1008@users.noreply.github.com>
2021-09-08 09:58:57 -0400

    remove build directory tracking for variableblock

--- blocks/variableblock/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'ea4f017a1f56c7f3672c86596e48dd21');
